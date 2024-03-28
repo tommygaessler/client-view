be0baf59241c8a321d41a1edbbccbee616b7bfc2
Tommy Gaessler <tommy.gaessler@gmail.com>
2024-03-28 11:48:42 -0600

    updated to 3.5.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DOCUMENT } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
@@ -15,25 +15,19 @@ ZoomMtg.prepareWebSDK();
 })
 export class AppComponent implements OnInit {
 
-  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
   signatureEndpoint = 'https://26040c9x85.execute-api.us-west-1.amazonaws.com/default/meetingsdk'
-  // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
-  sdkKey = ''
+  sdkKey = 'viodIoBwGbXY8HfsCADKrAIqOgdLuwLNPVUI'
   meetingNumber = ''
   role = 0
   leaveUrl = 'https://tommygaessler.com/client-view'
   userName = 'Zoom Meeting SDK'
   userEmail = ''
   passWord = ''
-  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
-  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
-  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
   registrantToken = ''
   zakToken = ''
 
-  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document, private activatedRoute: ActivatedRoute) {
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document, private activatedRoute: ActivatedRoute, private ngZone: NgZone) {
     this.activatedRoute.queryParams.subscribe(params => {
-      this.sdkKey = params['key'];
       this.meetingNumber = params['meeting'];
       this.passWord = params['passcode'];
     });
@@ -62,31 +56,33 @@ export class AppComponent implements OnInit {
 
     document.getElementById('zmmtg-root').style.display = 'block'
 
-    ZoomMtg.init({
-      leaveUrl: this.leaveUrl,
-      patchJsMedia: true,
-      success: (success) => {
-        console.log(success)
-        ZoomMtg.join({
-          signature: signature,
-          meetingNumber: this.meetingNumber,
-          userName: this.userName,
-          sdkKey: this.sdkKey,
-          userEmail: this.userEmail,
-          passWord: this.passWord,
-          tk: this.registrantToken,
-          zak: this.zakToken,
-          success: (success) => {
-            console.log(success)
-          },
-          error: (error) => {
-            console.log(error)
-          }
-        })
-      },
-      error: (error) => {
-        console.log(error)
-      }
+    this.ngZone.runOutsideAngular(() => {
+      ZoomMtg.init({
+        leaveUrl: this.leaveUrl,
+        patchJsMedia: true,
+        success: (success) => {
+          console.log(success)
+          ZoomMtg.join({
+            signature: signature,
+            meetingNumber: this.meetingNumber,
+            userName: this.userName,
+            sdkKey: this.sdkKey,
+            userEmail: this.userEmail,
+            passWord: this.passWord,
+            tk: this.registrantToken,
+            zak: this.zakToken,
+            success: (success) => {
+              console.log(success)
+            },
+            error: (error) => {
+              console.log(error)
+            }
+          })
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      })
     })
   }
 }
